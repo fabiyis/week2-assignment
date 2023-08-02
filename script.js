8352ab54defd992ca0464d8a78a0fa431b4321c5
fabiyis <141014211+fabiyis@users.noreply.github.com>
2023-08-02 07:45:38 -0700

    create pseudocode for calculating  age

---
+++ script.js
@@ -0,0 +1,13 @@
+/* declare variables:
+ birthYear, currentYear, age; */ 
+
+ /* input birth year and current year:
+ input="enter your birth year: ", birthYear;
+ input="enter your current year: ", currentYear; */
+
+ /* calculate age:
+ age=currentYear - birthYear; */
+
+/* output of the age:
+output="your age is: ", age; */
+
