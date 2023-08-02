377c183577fae28c578c4a77ff1046321356cc0c
fabiyis <141014211+fabiyis@users.noreply.github.com>
2023-08-02 08:01:17 -0700

    create a pseudocode to loop through an array for even numbers

--- script.js
+++ script.js
@@ -11,3 +11,13 @@
 /* output of the age:
 output="your age is: ", age; */
 
+/* declare arrays of numbers:
+[1,2,3,4,5,6,7,8,9,10];
+
+loop through the array and output even numbers:
+FOR EACH number IN array
+    IF number MOD 2 = 0 THEN
+        OUTPUT number
+    END IF...
+END FOR EACH
+*/
